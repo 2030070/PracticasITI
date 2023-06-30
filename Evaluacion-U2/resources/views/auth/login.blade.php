1f7ae47e2eaf34d9ac6ceba233ccc665d5a00ad4
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-06-30 01:16:40 -0600

    Cambios

--- Evaluacion-U2/resources/views/auth/login.blade.php
+++ Evaluacion-U2/resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
 
 @section('contenido')
     <div class="flex justify-center items-center" style="height: 70vh">
-        <div class="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-lg bg-blue-50 shadow-lg p-8">
-            <h1 class="text-center text-3xl font-bold mb-12 text-blue-600">Iniciar sesión</h1>
+        <div class="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-lg bg-blue-50 p-6 shadow-xl" >
+            <h1 class="text-center text-3xl font-bold mb-12 text-blue-600 ">Iniciar sesión</h1>
 
             <form method="POST" action="{{route('login')}}" class="space-y-4">
                 @csrf
@@ -13,7 +13,7 @@
                         {{session('mensaje')}}
                     </p>
                 @endif
-                <div>
+                <div class="mb-5">
                     <div class="flex items-center bg-blue-500 rounded-lg">
                         {{-- <label for="email" class="block ml-2">Email</label> --}}
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256">
@@ -23,7 +23,7 @@
                         class="w-full p-2 pl-8 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 @error('email') border-red-500 @enderror" value="{{old('email')}}" />
                     </div>
                 </div>
-                <div>
+                <div class="mb-5">
                     <div class="flex items-center bg-blue-500 rounded-lg">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256">
                             <path d="M208,80H176V56a48,48,0,0,0-96,0V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80ZM96,56a32,32,0,0,1,64,0V80H96ZM208,208H48V96H208V208Zm-68-56a12,12,0,1,1-12-12A12,12,0,0,1,140,152Z"></path>
