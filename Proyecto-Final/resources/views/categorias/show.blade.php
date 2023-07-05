897f377128a647b65b4da82b8b7ff4e617a594c8
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-07-04 22:13:33 -0600

    Btn eliminar

--- Proyecto-Final/resources/views/categorias/show.blade.php
+++ Proyecto-Final/resources/views/categorias/show.blade.php
@@ -5,28 +5,33 @@
 @endsection
 
 @section('contenido')
-<div class="container mx-auto px-4  ">
-    <div class="overflow-x-auto  py-20">
-        <table class="min-w-full border border-gray-300">
+<div class="container mx-auto px-4">
+    <div class="overflow-x-auto py-20">
+        <table class="min-w-full border-2 border-blue-500 rounded-lg">
             <thead>
                 <tr>
-                    <th class="py-2 px-4 border-b">Código</th>
-                    <th class="py-2 px-4 border-b">Descripción</th>
-                    <th class="py-2 px-4 border-b">Creado por</th>
-                    <th class="py-2 px-4 border-b">Acciones</th>
+                    <th class="py-2 px-4 border-b text-left">Código</th>
+                    <th class="py-2 px-4 border-b text-left">Descripción</th>
+                    <th class="py-2 px-4 border-b text-left">Creado por</th>
+                    <th class="py-2 px-4 border-b text-left">Acciones</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($categorias as $categoria)
                 <tr>
-                    <td class="py-2 px-4 border-b">{{ $categoria->codigo }}</td>
-                    <td class="py-2 px-4 border-b">{{ $categoria->descripcion }}</td>
-                    <td class="py-2 px-4 border-b">{{ $categoria->creado_por }}</td>
-                    <td class="py-2 px-4 border-b">
+                    <td class="py-2 px-4 border-b text-right">{{ $categoria->codigo }}</td>
+                    <td class="py-2 px-4 border-b text-left">{{ $categoria->descripcion }}</td>
+                    <td class="py-2 px-4 border-b text-left">{{ $categoria->creado_por }}</td>
+                    <td class="py-2 px-4 border-b text-left">
                         <form action="{{ route('categorias.destroy', $categoria->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="text-red-600">Eliminar</button>
+                            <button type="submit" class="text-red-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256">
+                                    <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z">
+                                    </path>
+                                </svg>
+                            </button>
                         </form>
                     </td>
                 </tr>
