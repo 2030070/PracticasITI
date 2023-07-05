9033347f47bbbbf1534f3f9d6cb6750c4a9e256d
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-07-04 20:49:59 -0600

    Migraciones

---
+++ Proyecto-Final/database/migrations/2023_07_05_021715_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoria_id')->constrainted('categorias');
+            $table->foreignId('subcategoria_id')->constrainted('subcategorias');
+            $table->decimal('precio_compra', 8, 2);
+            $table->decimal('precio_venta', 8, 2);
+            $table->integer('unidades_disponibles');
+            $table->string('creado_por');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
