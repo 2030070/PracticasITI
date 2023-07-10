109ea4a2b8926dfe76c1aad64714ec2c0b221621
César Aldahir Flores Gámez <72424254+2030070@users.noreply.github.com>
2023-07-09 18:10:57 -0600

    Cmabiando estilo del menu

--- Proyecto-Final/resources/views/layouts/app.blade.php
+++ Proyecto-Final/resources/views/layouts/app.blade.php
@@ -26,6 +26,10 @@
         @vite('resources/js/argon-dashboard-tailwind.js')
         @vite('resources/js/argon-dashboard-tailwind.min.js')
         @vite('resources/js/navbar-sticky.js')
+
+        <style>
+            
+        </style>
     </head>
     {{-- Cuerpo principal --}}
     <body class="m-0 font-sans text-base antialiased font-normal leading-default h-full">
@@ -34,17 +38,30 @@
             @yield('nav')
             @auth
             <!-- sidenav -->
-                <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0 " aria-expanded="false">
-                    <div class="h-19 py-14">
-                        <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
+                <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-blue-50 border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
+                    
+                    <div class="h-8 py-14">
+                        <ul class="flex flex-col pl-0 mb-0">
+                            <li class="mt-0.5 w-full">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('post_index') }}">
+                                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                        <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-columns"></i>
+                                    </div>
+                                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <i class=" h-6 absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
+                        
                     </div>
-
+                     
                     <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
 
                     <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
                         <ul class="flex flex-col pl-0 mb-0">
+                            
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('productos.create') }}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('productos.create') }}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-box-open"></i>
                                     </div>
@@ -53,7 +70,7 @@
                             </li>
 
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('categorias.create') }}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('categorias.create') }}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-folder-plus"></i>
                                     </div>
@@ -61,7 +78,7 @@
                                 </a>
                             </li>
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('subcategorias.create') }}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('subcategorias.create') }}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-folder-plus"></i>
                                     </div>
@@ -69,7 +86,7 @@
                                 </a>
                             </li>
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('marcas.create') }}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('marcas.create') }}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-folder-plus"></i>
                                     </div>
@@ -77,7 +94,7 @@
                                 </a>
                             </li>
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{route('ventas.create')}}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{route('ventas.create')}}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-folder-plus"></i>
                                     </div>
@@ -85,30 +102,35 @@
                                 </a>
                             </li>
                             <li class="mt-0.5 w-full">
-                                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('devoluciones.create') }}">
+                                <a class="py-2.7  dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors" href="{{ route('devoluciones.create') }}">
                                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                         <i class="relative top-0 text-sm leading-normal text-blue-500 fas fa-folder-plus"></i>
                                     </div>
                                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Devoluciones</span>
                                 </a>
                             </li>
-                            <li class="mt-0.5 w-full">
-                                <form method="POST" action="{{route('logout')}}">
-                                    @csrf
-                                    <button type="submit">
-                                        <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors">
-                                            <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                                                <i class="relative top-0 text-sm leading-normal text-blue-500  fas fa-sign-out-alt "></i>
-                                            </div>
-                                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Cerrar Sesion</span>
-                                        </a>
-                                    </button>
-                                </form>
-                            </li>
+                            
                         </ul>
                     </div>
 
                     <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
+                    
+                    <ul class="flex flex-col pl-0 mb-0">
+                        <li class="mt-0.5 w-full">
+                            <form method="POST" action="{{route('logout')}}">
+                                @csrf
+                                <button type="submit">
+                                    <a class="py-2.7 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors">
+                                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                            <i class="relative top-0 text-sm leading-normal text-blue-500  fas fa-sign-out-alt "></i>
+                                        </div>
+                                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Cerrar Sesion</span>
+                                    </a>
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                
                 </aside>
             <!-- end sidenav -->
             @endauth
@@ -126,4 +148,33 @@
             </div>
         </footer>
     </body>
+
+    <script>
+        // Obtener todas las opciones del menú
+        const menuOptions = document.querySelectorAll('aside ul li');
+    
+        // Agregar el evento de escucha para cada opción del menú
+        menuOptions.forEach(option => {
+            option.addEventListener('mouseover', () => {
+                option.classList.add('hover:scale-95');
+            });
+    
+            option.addEventListener('mouseout', () => {
+                option.classList.remove('hover:scale-95');
+            });
+        });
+    
+        // Obtener la URL actual
+        const currentUrl = window.location.href;
+    
+        // Comprobar la URL actual y marcar la opción seleccionada
+        menuOptions.forEach(option => {
+            const link = option.querySelector('a');
+    
+            if (link.href === currentUrl) {
+                option.classList.add('bg-blue-500/13');
+                option.classList.add('rounded-lg');
+            }
+        });
+    </script>
 </html>
